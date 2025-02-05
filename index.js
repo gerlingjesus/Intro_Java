d1e590cbad346e774d0448c7734856bb3e8ccf2f
gerlingjesus <gerlingperez96@gmail.com>
2025-02-04 21:20:55 -0600

    Subiendo archivo a trabajar

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log('Hola desde JS');
+
+// alert('tu password incorrecto');
+// console.log(typeof nombre)
+
+//var nombre = 'Gerling'
+// var nombre = prompt('Nombre:')
+// var edad = 28
+// var gusto1 = 'camina'
+
+// /*console.log(nombre + edad)
+// console.log('mi nombre es:',nombre, 'Mi edad es:', edad)
+// */
+
+// //console.log(`Mi nombre es: ${nombre}, tengo la edad de: ${edad}`)
+// document.write(`Mi nombre es: ${nombre}, tengo la edad de: ${edad}`)
+
+// document.write('Coloca tu nombre:')
+
+// var numero1 = parseInt(prompt('Numero 1:'))
+// var numero2 = parseInt(prompt('Numero 2:'))
+// var numero3 = parseInt(prompt('Numero 3:'))
+var numero1 = Number(prompt('Numero 1:'))
+var numero2 = Number(prompt('Numero 2:'))
+var numero3 = Number(prompt('Numero 3:'))
+
+var suma = numero1+numero2+numero3;
+
+//alert('suma' + suma)
+
+document.write(suma)
