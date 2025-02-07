477d317f57f89da94269449018babe1bdf667551
gerlingjesus <gerlingperez96@gmail.com>
2025-02-06 23:26:57 -0600

    Subiendo  Evaluador de Notas con Mensajes Personalizados 060225

--- tipos-de-datos.js
+++ tipos-de-datos.js
@@ -1,22 +1,8 @@
-// console.log('Hola desde JS');
-
-// alert('tu password incorrecto');
-// console.log(typeof nombre)
-
-//var nombre = 'Gerling'
-// var nombre = prompt('Nombre:')
-// var edad = 28
-// var gusto1 = 'camina'
-
-// /*console.log(nombre + edad)
-// console.log('mi nombre es:',nombre, 'Mi edad es:', edad)
-// */
-
+//ACTIVIDAD 01
 // //console.log(`Mi nombre es: ${nombre}, tengo la edad de: ${edad}`)
 // document.write(`Mi nombre es: ${nombre}, tengo la edad de: ${edad}`)
 
-// document.write('Coloca tu nombre:')
-
+/*
 console.log(typeof 42)
 console.log(typeof 'veinticinco')
 console.log(typeof -666)
@@ -25,11 +11,8 @@ console.log(typeof 0)
 console.log(typeof'')
 console.log(typeof null)
 console.log(typeof undefined)
-console.log(typeof false)
-
-// var numero1 = parseInt(prompt('Numero 1:'))
-// var numero2 = parseInt(prompt('Numero 2:'))
-// var numero3 = parseInt(prompt('Numero 3:'))
+console.log(typeof false) */
+/*
 var numero1 = Number(prompt('Numero 1:'))
 var numero2 = Number(prompt('Numero 2:'))
 var numero3 = Number(prompt('Numero 3:'))
@@ -38,4 +21,45 @@ var suma = numero1+numero2+numero3;
 
 //alert('suma' + suma)
 
-document.write(suma)
+document.write(suma) */
+
+
+//ACTIVIDAD 2: Evaluador de Notas con Mensajes Personalizados
+// Crea un programa en JavaScript que evalúe la nota de un estudiante y genere un mensaje personalizado basado en la nota. Las notas se asignan de la siguiente manera:
+
+// Si la nota es 90 o más, el estudiante aprueba con "Excelente".
+// Si la nota está entre 75 y 89, el estudiante aprueba con "Bien".
+// Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
+// Si la nota es menor de 60, el estudiante no aprueba.
+
+let Nota = Number(prompt("Ingresa tu nota:"));
+
+if(Nota !== 0){
+// Si la nota es 90 o más, el estudiante aprueba con "Excelente".
+    if(Nota >= 90 && Nota <=100){
+            document.write("Excelente");
+        }
+// Si la nota está entre 75 y 89, el estudiante aprueba con "Bien".
+    else if(Nota >= 75 && Nota <=89){
+        document.write("Bien");
+    }
+// Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
+    else if(Nota >= 60 && Nota <=74){
+        document.write("Suficiente");
+    }
+// Si la nota es menor de 60, el estudiante no aprueba.
+    else if(Nota < 60){
+        document.write("No aprobatorio");
+    }
+    else{
+        document.write("Numero invalido :(");
+    }
+
+
+
+
+
+}
+else{
+    document.write("INGRESA UN NUMERO DIFERENTE A 0")
+}
